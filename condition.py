e0cbb8027b28e29a24a025d0f8235773e9073e4e
Akriti Singh <94702913+akritis8423@users.noreply.github.com>
2022-06-22 23:06:14 +0530

    Delete condition.py

--- condition.py
+++
@@ -1,7 +0,0 @@
-radius=int(input("enter the radius of the circle"))
-if radius >=0:
-    area=radius*radius*3.14
-    print("area is", area)
-else:
-    print("negative value for radius is not acceptable")
-
